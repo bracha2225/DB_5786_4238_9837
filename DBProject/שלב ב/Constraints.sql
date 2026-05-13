a224672c7d7420671fc75fc4e9d306767998625e
Guilacz <guilacz03@gmail.com>
2026-05-13 10:59:18 +0300

    end shlav 2

---
+++ DBProject/שלב ב/Constraints.sql
@@ -0,0 +1,8 @@
+ALTER TABLE patient
+ADD CONSTRAINT check_email_at CHECK (email LIKE '%@%');
+
+ALTER TABLE insurance
+ADD CONSTRAINT check_insurance_valid_date CHECK (expiration_date > '2000-01-01');
+
+ALTER TABLE emergency_contact
+ADD CONSTRAINT check_relationship_type CHECK (relationship IN ('Parent', 'Spouse', 'Child', 'Sibling', 'Friend', 'Other'));
---
+++ DBProject/שלב ב/RollbackCommit.sql
@@ -0,0 +1,26 @@
+--roll back
+
+SELECT name FROM emergency_contact WHERE contact_id = 1;
+
+BEGIN;
+
+UPDATE emergency_contact SET name = 'NOM_TEST_ERREUR' WHERE contact_id = 1;
+
+SELECT name FROM emergency_contact WHERE contact_id = 1;
+
+ROLLBACK;
+
+SELECT name FROM emergency_contact WHERE contact_id = 1;
+
+-- commit
+SELECT patient_id, address FROM patient WHERE patient_id = 1;
+
+BEGIN;
+
+UPDATE patient
+SET address = '123 New Permanent Street'
+WHERE patient_id = 1;
+
+COMMIT;
+
+SELECT patient_id, address FROM patient WHERE patient_id = 1;
