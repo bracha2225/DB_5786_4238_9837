094dc39cbfae8af46d1f0b592de4f21d37fdff13
Guilacz <guilacz03@gmail.com>
2026-05-13 11:09:16 +0300

    add backup

--- DBProject/שלב ב/Constraints.sql
+++ DBProject/שלב ב/Constraints.sql
@@ -1,6 +1,6 @@
 ALTER TABLE patient
 ADD CONSTRAINT check_email_at CHECK (email LIKE '%@%');
-
+--
 ALTER TABLE insurance
 ADD CONSTRAINT check_insurance_valid_date CHECK (expiration_date > '2000-01-01');
 
